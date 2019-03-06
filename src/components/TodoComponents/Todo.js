e6ac0168004207ca594e7ae2117d88900401f68f
Dexter Lewis <dexter.lewis@gmail.com>
2019-03-05 23:33:18 -0500

    Fixed code formatting and spacing

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./Todo.css";
 
 function Todo(props) {
-    
   return (
-      <div className="card">
+    <div className="card">
       <div className="card-content">
-      <input type="checkbox" checked={props.todo.completed ? "checked" : ""} onChange={() => props.taskComplete(props.todo.id)}/>
-    <span
-      onClick={() => props.taskComplete(props.todo.id)}
-      className={props.todo.completed ? "task-complete" : "task-open"}
-    >
-      {props.todo.task}
-      
-    </span>
-    </div>
+        <input
+          type="checkbox"
+          checked={props.todo.completed ? "checked" : ""}
+          onChange={() => props.taskComplete(props.todo.id)}
+        />
+        <span
+          onClick={() => props.taskComplete(props.todo.id)}
+          className={props.todo.completed ? "task-complete" : "task-open"}
+        >
+          {props.todo.task}
+        </span>
+      </div>
     </div>
   );
 }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -5,20 +5,12 @@ import Todo from "./Todo";
 import "./Todo.css";
 
 function TodoList(props) {
-    
-
-    
   return (
-    // <Todo
-    //   id={props.item.id}
-    //   item={props.item.task}
-    //   task={props.item.completed}
-    //   taskComplete={props.taskComplete}
-    // />
     <div>
-        {props.items.map(item => <Todo key={item.id} todo={item} taskComplete = {props.taskComplete}/>)}
+      {props.items.map(item => (
+        <Todo key={item.id} todo={item} taskComplete={props.taskComplete} />
+      ))}
     </div>
-
   );
 }
 
